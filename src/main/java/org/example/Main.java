0667cc8f53eae6fca42e4b041ab0fe7f7b149f4b
Mateus <mateusfigueredo037@gmail.com>
2023-10-01 02:16:06 -0300

    small change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("------JOGO DA VELHA------");
+        System.out.println("-----JOGO DA VELHA-----");
         System.out.println();
         System.out.print("Digite o nome do jogador 1: ");
         scan.next();
